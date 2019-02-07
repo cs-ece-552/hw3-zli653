b43282a4a5e6d2614ff350fdf2706e22fd7b5b8f
Matt Sinclair <20143235+mattsinc@users.noreply.github.com>
2019-02-07 01:16:32 -0600

    Problem 2 files + move Problem 1 to separate folder

---
+++ hw3_2/rca_16b.v
@@ -0,0 +1,18 @@
+/*
+    CS/ECE 552 Spring '19
+    Homework #3, Problem 2
+    
+    a 16-bit RCA module
+*/
+module rca_16b(A, B, S, C_out);
+
+    // declare constant for size of inputs, outputs (N)
+    parameter   N = 16;
+
+    input [N-1: 0] A, B;
+    output [N-1:0] S;
+    output         C_out;
+
+	// YOUR CODE HERE
+
+endmodule
