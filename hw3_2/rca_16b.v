05771a3ae9096fd25941be1eacfd0ef134e17b9f
Matt Sinclair <20143235+mattsinc@users.noreply.github.com>
2019-02-09 18:01:20 -0600

    Update rca_16b.v
    C_in is an input for rca_16b, was mistakenly not included.

--- hw3_2/rca_16b.v
+++ hw3_2/rca_16b.v
@@ -4,12 +4,13 @@
     
     a 16-bit RCA module
 */
-module rca_16b(A, B, S, C_out);
+module rca_16b(A, B, C_in, S, C_out);
 
     // declare constant for size of inputs, outputs (N)
     parameter   N = 16;
 
     input [N-1: 0] A, B;
+    input          C_in;
     output [N-1:0] S;
     output         C_out;
 
